4aa286fdf991dd549b5803f37c54ba8b7458b87b
loomsden <84926563+loomsden@users.noreply.github.com>
2021-06-06 15:14:44 -0500

    Add files via upload

---
+++ searchReward.js
@@ -0,0 +1,38 @@
+module.exports = function(){
+	var express = require('express');
+	var router = express.Router();
+
+	function searchRewards(res, db, context, term, complete){
+		db.pool.query("SELECT * FROM Rewards WHERE rName LIKE '%" + term + "%' OR type LIKE '%" + term + "%' OR rarity LIKE '%" + term + "%' ORDER BY rewardID ASC;", function(error, results, fields){
+			if (error){
+				res.write(JSON.stringify(error));
+				res.end(); 
+			}
+			context.rewards = results;
+			complete();
+		});
+	}
+
+
+	router.post('/', function(req, res){
+		var callbackCount = 0;
+		var context = {};
+		context.jsscripts = ["deletereward.js"];
+		var db = req.app.get('mysql');
+		
+		var term = req.body.search;
+        
+		searchRewards(res, db, context, term, complete);
+		
+		function complete(){
+			callbackCount++;
+			if (callbackCount >= 1)
+			{
+				res.render('reward', context);
+				//res.send(JSON.stringify(context));
+			}
+		}
+	});
+	
+	return router;
+}();
