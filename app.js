726704101c847f4a4a0fbcaa7f73d3983d4ee9a2
loomsden <84926563+loomsden@users.noreply.github.com>
2021-06-06 17:55:19 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var db = require('./db-connector');
 var bodyParser = require('body-parser');
 var path = require('path');
 var alert = require('alert');
-import alert from 'alert'
 
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
